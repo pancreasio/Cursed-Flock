7b9584ee6bb5e9139da558dbed5a9d538f6ef860
pancreasio <julianholisu@gmail.com>
2020-11-30 01:13:12 -0300

    alignment behaviour implemented

---
+++ CursedFlock/Assets/Scripts/AlignmentBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignmentBehaviour : BoidBehaviour
+{
+    public override Vector3 GetBehaviourForce(Boid targetBoid, List<Boid> neighborBoids, List<GameObject> threatList, List<GameObject> foodList, Vector3 nextWaypointPosition)
+    {
+        Vector3 resultantVelocity = Vector3.zero;
+        int neighbourCount = 0;
+        foreach (Boid neighbor in neighborBoids)
+        {
+            if (neighbor == targetBoid)
+                continue;
+
+            if ((targetBoid.transform.position - neighbor.transform.position).magnitude < effectiveRange)
+            {
+                resultantVelocity += neighbor.transform.position;
+                neighbourCount++;
+            }
+        }
+
+        if (neighbourCount > 0)
+        {
+            resultantVelocity = resultantVelocity.normalized * targetBoid.maxSpeed;
+            return resultantVelocity;
+        }
+        return base.GetBehaviourForce(targetBoid, neighborBoids, threatList, foodList, nextWaypointPosition);
+    }
+}
